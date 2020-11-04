dc0fa746052ef4e7cf48b2b986e824d4d122028b
Cameron Brown <cameron-brown@live.com>
2020-11-04 20:03:32 +0000

    sepereated the connect funtion to play.

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-// PORT: 50542
-// HOST: 135.23.222.131
-
-const net = require('net');
-//Establish connection with game server
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
-  //interpret incoming data as text
-  conn.setEncoding('utf8')
-  conn.on('data', (data) => {
-    console.log('Message from server: ', data);
-  });
-  return conn;
-}
+const { connect } = require('./client')
 
 console.log('Connecting...')
 connect()
