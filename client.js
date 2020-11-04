5c6870398fe28a8d4f2872983b1d65a066d7865b
cambrown101 <cameron-brown@live.com>
2020-11-04 12:39:48 -0800

    started adding the ability to move

--- client.js
+++ client.js
@@ -9,6 +9,10 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server')
     conn.write("Name: CAM");
+    // conn.write("Move: up");
+    // conn.write("Move: down");
+    // conn.write("Move: left");
+    // conn.write("Move: right");
   })
   conn.on('data', (data) => {
     console.log('Message from server: ', data);
