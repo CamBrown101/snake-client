f3ffc7102f4aee19526e441894186bf9f03ecc14
cambrown101 <cameron-brown@live.com>
2020-11-04 13:45:39 -0800

    added WASD movement

--- input.js
+++ input.js
@@ -1,18 +1,30 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  handleUserInput(stdin);
+  handleUserInput(stdin, conn);
   return stdin;
 };
 
-const handleUserInput = (stdin) => {
+const handleUserInput = (stdin, conn) => {
   stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
     }
+    if (key === 'w') { conn.write("Move: up") };
+    if (key === 's') { conn.write("Move: down") };
+    if (key === 'a') { conn.write("Move: left") };
+    if (key === 'd') { conn.write("Move: right") };
   });
 };
+
+// conn.write("Move: up");
+// conn.write("Move: down");
+// conn.write("Move: left");
+// conn.write("Move: right");
 
 module.exports = { setupInput };
